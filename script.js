5eb61ec94ea8509e2f443c1ced77bb8d707a4b6a
Luca <107370683+LucaZepponi@users.noreply.github.com>
2022-12-26 21:58:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+h1 {
+  color: red;
+}
+
+.towers-container  {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.towers {
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+}
+
+.heightDisks {
+  /*display: flex;
+  flex-direction: column;
+  align-items: center;*/
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.base {
+  background-color: gray;
+}
+
+.height-container {
+  display: flex;
+  justify-content: space-around;
+}
+
+.height {
+  display: flex;
+  background-color: gray;
+  align-items: center;
+  justify-content: center;
+  
+  background-color: gray;
+}
+
+.disk {
+  height: 0.6cm;
+
+  margin: 0 auto;
+
+  background-color: orange;
+  border-radius: 6px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
